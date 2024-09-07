568da102fb4d36cddec7fafb3277a9c45b3b7d6d
kingmalitha <sandaruwanmalitha2018@gmail.com>
2024-09-07 14:16:16 +0530

    feat: chat and profile page

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const ChatPage = () => {
+import { images } from "@/constants/data";
+import tw from "@/lib/tw";
+
+const Chat = () => {
   return (
-    <View>
-      <Text>ChatPage</Text>
-    </View>
+    <SafeAreaView style={tw.style("flex-1 bg-white p-5")}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Text style={tw.style("text-2xl font-JakartaBold")}>Chat</Text>
+        <View style={tw.style("flex-1 h-fit flex justify-center items-center")}>
+          <Image
+            source={images.message}
+            alt="message"
+            style={tw.style("w-full h-40")}
+            resizeMode="contain"
+          />
+          <Text style={tw.style("text-3xl font-JakartaBold mt-3")}>
+            No Messages Yet
+          </Text>
+          <Text style={tw.style("text-base mt-2 text-center px-7")}>
+            Start a conversation with your friends and family
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
-export default ChatPage;
+export default Chat;
